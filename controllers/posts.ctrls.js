37454a7eb012c6979cd1ce09a4ba204c9732aeec
jbep <joshuabemartin@gmail.com>
2022-11-02 21:39:42 -0400

    get route working on postman

--- controllers/posts.ctrls.js
+++ controllers/posts.ctrls.js
@@ -2,7 +2,15 @@
 const db = require("../models")
 //posts get route
 const index = (req, res) => {
-  res.send("get route working")
+  // res.send("get route working")
+  db.Post.find({}, (err,
+    posts) => {
+      if(err) return res.status(404).json({error: err.message})
+      return res.status(200).json({
+        posts,
+        requestedAt: new Date().toLocalDateString()
+      })
+    })
 }
 
 
