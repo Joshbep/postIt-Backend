ed00d9ff8a5bd490c1c9ce1d2c085d34750682c0
jbep <joshuabemartin@gmail.com>
2022-11-02 20:03:59 -0400

    res.send works for posts in cntrls

---
+++ controllers/posts.ctrls.js
@@ -0,0 +1,10 @@
+
+//posts get route
+const index = (req, res) => {
+  res.send("get route working")
+}
+
+
+module.exports = {
+  index
+}
--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require ("express")
 const app = express();
 
+// internal modules
+const routes = require('./routes')
+
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3001
@@ -10,6 +13,9 @@ const PORT = process.env.PORT || 3001
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+
+// Routes
+app.use("/posts", routes.posts)
 
 app.listen (PORT, () => {
     console.log(" Connected! ")
