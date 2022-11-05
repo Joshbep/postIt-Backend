1188a9d84f645f1e6e09f8c692258fa55344771b
jbep <joshuabemartin@gmail.com>
2022-11-05 12:53:26 -0400

    signin and signout route works in postman

--- controllers/users.ctrls.js
+++ controllers/users.ctrls.js
@@ -15,7 +15,30 @@ const register = (req, res) => {
   })
 }
 
+const signin = (req, res) => {
+  console.log(req.body)
+  db.User.findOne({username: req.body.username}, (err, foundUser) => {
+    if(foundUser) {
+      console.log(foundUser)
+      const validLogin = bcrypt.compareSync(req.body.password, foundUser.password)
+      if (validLogin) {
+        res.json(foundUser)
+    } else {
+      res.send('Invalid username or password')
+    }
+  }
+  })
+}
+
+const signout = (req, res) => {
+  req.session.destroy(() => {
+      res.status(200).json({ msg: 'users logged out' })
+  })
+}
+
 
 module.exports = {
-   register
+   register,
+   signin,
+   signout
 }
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const ctrls = require("../controllers")
 
+router.post("/signin", ctrls.users.signin)
 router.post("/register", ctrls.users.register)
-
+router.delete("/signout", ctrls.users.signout)
 module.exports = router;
