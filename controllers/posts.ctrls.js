ba76b0531fd3ef98bd4329972e05b6eb9e91bb05
jbep <joshuabemartin@gmail.com>
2022-11-05 16:26:58 -0400

    like and dislike routes work now

--- controllers/posts.ctrls.js
+++ controllers/posts.ctrls.js
@@ -51,7 +51,7 @@ const update = (req, res) => {
 //like and dislike a post
 const like = async (req, res) => {
   try {
-    const post = db.Post.findById(req.params.id);
+    const post = await db.Post.findById(req.params.id);
     if(!post.likes.includes(req.body.userId)) {
       await post.updateOne({ $push: { likes: req.body.userId } });
       res.status(200).json("the post has been liked")
