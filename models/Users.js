1029d5813b6b970ba13d2d3de561995ab68602d9
jbep <joshuabemartin@gmail.com>
2022-11-14 19:27:11 -0500

    updated user model

--- models/Users.js
+++ models/Users.js
@@ -1,17 +1,15 @@
 const mongoose = require ("mongoose")
 
 const UserSchema = new mongoose.Schema({
-    username: {type: String, required:true},
+    username: {type: String, required:true, unique: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required:true, min: 5},
-    profilePicture: {
-      type: String,
-      default: "",
-    },
+    description: {type: String},
+    profilePicture: {type: String, default: "",},
     coverPicture: {type: String, default:""},
     followers: {type: Array, default: []},
     following: {type: Array, default: []},
-    isAdmin: {type: Boolean, default: false}
+    isAdmin: {type: Boolean, default: false},
 },
 { timestamps: true }
 )
